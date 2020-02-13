3c2a422069960ba7ec8c028171d0e0a4ae89f1f5
jareckikck <jaroslawgrabarczyk01@gmail.com>
2020-02-13 12:18:15 +0100

    scrap all search queries at once

--- scrap.js
+++ scrap.js
@@ -5,40 +5,43 @@ const puppeteer = require("puppeteer");
 firebase.initializeApp(config.firebase);
 
 let db = firebase.firestore();
-let scrapResults = db.collection("scrapResults");
-let scrapSiteConfig = db.collection("scrapSiteConfig");
+let collectionScrapResults = db.collection("scrapResults");
+let collectionScrapSiteConfig = db.collection("scrapSiteConfig");
+let collectionSearchQuery = db.collection("searchQuery");
+
+let providers = new Map();
+let queries;
+let runIterator = 0;
 
 loginToFirebase();
+start();
 
-firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-    loadConfigAndScrap("allegro");
-  }
-});
-
-function getDate() {
-  const today = new Date();
-  const dd = String(today.getDate()).padStart(2, "0");
-  const mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
-  const yyyy = today.getFullYear();
-
-  return mm + "." + dd + "." + yyyy;
-}
-
-function loadConfigAndScrap(name) {
-  scrapSiteConfig
-    .where("name", "==", name)
-    .get()
-    .then(function(querySnapshot) {
-      querySnapshot.forEach(function(doc) {
-        console.log("doc data retrived");
-        allegro = doc.data();
-        scrapData(allegro);
+async function start() {
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      loadSearchQueries().then(res => {
+        queries = res;
+        queries.forEach(doc => {
+          if (providers.has(doc.provider)) return;
+          providers.set(doc.provider, null); 
+          loadConfig(doc.provider)
+            .then(config => {
+              providers.set(doc.provider, config);
+            })
+            .then(() => {
+              queries.forEach(q => {
+                query = {
+                  provider: providers.get(q.provider),
+                  searchQuery: q.searchQuery
+                };
+                scrapData(query);
+                runIterator++;
+              });
+            });
+        });
       });
-    })
-    .catch(function(error) {
-      console.log("Error getting documents: ", error);
-    });
+    }
+  });
 }
 
 function loginToFirebase() {
@@ -53,14 +56,26 @@ function loginToFirebase() {
     });
 }
 
+async function loadSearchQueries() {
+  const snapshot = await collectionSearchQuery.get();
+  return snapshot.docs.map(doc => doc.data());
+}
+
+async function loadConfig(id) {
+  const snapshot = await collectionScrapSiteConfig.doc(id).get();
+  return snapshot.data();
+}
+
 function saveData(id, data) {
   console.log("start SaveData");
-  scrapResults
+  collectionScrapResults
     .doc(id)
     .set(data)
     .then(function() {
       console.log("Document successfully written!");
-      process.exit();
+      runIterator--;      
+      if (runIterator !== 0) return
+			process.exit();      
     })
     .catch(function(error) {
       console.error("Error writing document: ", error);
@@ -68,7 +83,7 @@ function saveData(id, data) {
     });
 }
 
-function scrapData(allegro) {
+function scrapData({ provider, searchQuery }) {
   try {
     (async () => {
       let queryResult = [];
@@ -77,26 +92,26 @@ function scrapData(allegro) {
       const browser = await puppeteer.launch();
       const page = await browser.newPage();
       await page.setViewport({ width: 1280, height: 800 });
-      await page.goto(allegro.url);
+      await page.goto(provider.url);
 
       //close pop up
-      if ((await page.$(allegro.popUpAccClass)) !== null) {
+      if ((await page.$(provider.popUpAccClass)) !== null) {
         console.log("close pop up");
-        await page.click(allegro.popUpAccClass);
+        await page.click(provider.popUpAccClass);
       } else {
         console.log("pop up not found");
       }
 
       //search for query
-      await page.type(allegro.searchForm, allegro.searchQuery);
-      await page.click(allegro.searchInput);
+      await page.type(provider.searchForm, searchQuery);
+      await page.click(provider.searchInput);
 
       //get results
-      await page.waitForSelector(allegro.itemContainer);
-      const item = await page.$$(allegro.item);
-      for (let i = 0; i < allegro.resultsLimit; i++) {
-        let title = await item[i].$eval(allegro.itemTitle, t => t.innerText);
-        let price = await item[i].$eval(allegro.itemPrice, p => p.innerText);
+      await page.waitForSelector(provider.itemContainer);
+      const item = await page.$$(provider.item);
+      for (let i = 0; i < provider.resultsLimit; i++) {
+        let title = await item[i].$eval(provider.itemTitle, t => t.innerText);
+        let price = await item[i].$eval(provider.itemPrice, p => p.innerText);
         let position = {
           title: title,
           price: price
@@ -110,8 +125,8 @@ function scrapData(allegro) {
       //prepare firebase objec
       let firebaseData = {
         queryResult: queryResult,
-        searchQuery: allegro.searchQuery,
-        srappedFrom: allegro.name,
+        searchQuery: searchQuery,
+        srappedFrom: provider.name,
         date: getDate()
       };
 
@@ -122,4 +137,13 @@ function scrapData(allegro) {
     console.error(err);
     process.abort();
   }
+}
+
+function getDate() {
+  const today = new Date();
+  const dd = String(today.getDate()).padStart(2, "0");
+  const mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+  const yyyy = today.getFullYear();
+
+  return mm + "." + dd + "." + yyyy;
 }
